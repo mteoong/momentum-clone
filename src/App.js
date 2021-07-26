923837d618c8f11986f695f2ce87c14f5146ec70
Marc Teo Ong <mrc.teoong@gmail.com>
2021-07-26 11:34:48 +0800

    App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <h1> Hello World! </h1>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+        grid:[],
+        mouseClicked:false,
+        selected:"",
+        start_node:null,
+        end_node:null,
+        visited:0,
+        shortestPath:0,
+        number_of_nodes:0,
+    }
+  }
+  
+  render() {
+    const backgroundStyles = {
+      backgroundImage: `url( ${image} )`,
+      padding: "1%",
+      height: "100vh",
+      width: "100%",
+      backgroundPosition: "center center",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+    };
+    return (
+      <div style={backgroundStyles}>
+        <Title/>
+        <Toolbar/>
+      </div>
+    )
+  }
 }
 
 export default App;
